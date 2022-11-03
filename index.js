9aa9a5e1c02ae9dd51835f9235adc1dd692c4e34
Rahul Gupta <rahulgupta.ra.com@gmail.com>
2022-11-03 17:03:17 +0530

    updated naming convensions

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const fs = require('fs-extra');
 const path = require('path');
 const { exec } = require('child_process');
 
-// ? repo1 path and repo1 object apth
-let repo1Path = path.join('/home/rahul/dev/rahul/Task1/repo1')
-let repo1ObjPath = path.join(repo1Path,'object/repo1.json')
+// ? repo1 path and repo1 object path
+const REPO1PATH = path.join('/home/rahul/dev/rahul/Task1/repo1')
+const REPO1OBJECTPATH = path.join(REPO1PATH,'object/repo1.json')
 
 
 // * pre prcoess arguments function
@@ -21,12 +21,12 @@ function processArgs(){
 async function main(){
     const options = processArgs();
     try {
-        let result = await fs.readFile(repo1ObjPath,'utf8')
+        let result = await fs.readFile(REPO1OBJECTPATH,'utf8')
         result = JSON.parse(result);
         result.key1 = options.key1 || result.key1;
         result.key2 = options.key2 || result.key2;
-        await fs.writeJSON(repo1ObjPath,result)
-        exec(`cd ${repo1Path} && git commit -am 'pre relese' && npm version patch && git push origin master`, (err, stdout, stderr) => {
+        await fs.writeJSON(REPO1OBJECTPATH,result)
+        exec(`cd ${REPO1PATH} && git commit -am 'pre relese' && npm version patch && git push origin master`, (err, stdout, stderr) => {
             if (err) {
                 console.error(err)
             } else {
